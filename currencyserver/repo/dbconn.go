471cca3e949c6c9bf7f4b65b85631a117f61e190
Leonardo Gazio <leogazio@gmail.com>
2021-02-19 17:34:36 -0300

    feat: ADD - createIndexes function.

--- currencyserver/repo/dbconn.go
+++ currencyserver/repo/dbconn.go
@@ -2,15 +2,40 @@ package repo
 
 import (
 	"context"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/x/bsonx"
 )
 
 //Database stores MongoDB connection
 var Database *mongo.Database
 
-//Open connects to MongDB database.
+//Creates database indexes
+func createIndexes() (err error) {
+	collation := options.Collation{
+		Locale:   "en",
+		Strength: 2,
+	}
+
+	models := []mongo.IndexModel{
+		{
+			Keys:    bsonx.Doc{{Key: "code", Value: bsonx.Int32(1)}},
+			Options: options.Index().SetUnique(true).SetCollation(&collation),
+		},
+		{
+			Keys:    bsonx.Doc{{Key: "name", Value: bsonx.Int32(1)}},
+			Options: options.Index().SetUnique(true).SetCollation(&collation),
+		},
+	}
+
+	opts := options.CreateIndexes().SetMaxTime(10 * time.Second)
+	_, err = Database.Collection("currencies").Indexes().CreateMany(context.Background(), models, opts)
+	return err
+}
+
+//Open connects to MongoDB database.
 func Open() error {
 	ctx := context.Background()
 	db, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
@@ -18,5 +43,10 @@ func Open() error {
 		return err
 	}
 	Database = db.Database("cryptocurrency")
+
+	if err := createIndexes(); err != nil {
+		return err
+	}
+
 	return nil
 }
